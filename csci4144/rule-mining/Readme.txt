c61dec08e2255a41b217886cc12434c410edaa88
Parth Patel <58887946+akshu10@users.noreply.github.com>
2022-04-02 15:03:54 -0300

    Apriori alg

---
+++ csci4144/rule-mining/Readme.txt
@@ -0,0 +1,8 @@
+1. RuleMining.py is located in the following directory:
+    {patel4}/csci4144/a4/rule-mining/RuleMining.py
+    where patel4 is my csid.
+
+
+2. To execute this code you need to navigate the directory above and use the following command:
+    python RuleMining.py
+    The result will be printed to Rules.txt file in the same directory as above.
---
+++ csci4144/rule-mining/RuleMining.py
@@ -0,0 +1,211 @@
+import csv
+import itertools
+
+
+def read_data(file_name):
+    header = ""
+    file_data = []
+    with open(file_name, 'r', encoding="utf-8") as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        header = next(data)
+        for row in data:
+            file_data.append(row)
+    return file_data
+
+
+def write_data(file_name, data):
+    with open(file_name, 'a', encoding="utf-8") as text_file:
+        for line in data:
+            text_file.write(line)
+    return
+
+
+def first(data):
+    '''
+    Generates the frequency set
+    '''
+    dictionary = dict()
+
+    for row in data:
+        for item in row:
+            if item not in dictionary:
+                dictionary[item] = 1
+            else:
+                dictionary[item] += 1
+
+    dictionary2 = dict()
+
+    for k, v in dictionary.items():
+        if v >= round(float(min_sup) * len(data)):
+            dictionary2[k] = v
+
+    return dictionary2
+
+
+def check_item(item_set, transaction):
+    '''
+    Checks whether each item in item_set is present in transaction
+    '''
+    contained = False
+
+    for item in item_set:
+        if item in transaction:
+            contained = True
+        else:
+            contained = False
+            break
+
+    return contained
+
+
+def item_sets(candidateList, data):
+    '''
+    Generates frequent item sets given a candidate list and also prunes 
+    '''
+    candidate_Set = set(candidateList)
+
+    freq = dict()
+
+    for item in candidate_Set:
+        for transaction in data:
+            check = check_item(item, transaction)
+            if check:
+                if item in freq:
+                    freq[item] += 1
+                else:
+                    freq[item] = 1
+
+    prune = dict()
+
+    for k, v in freq.items():
+        if v >= round(float(min_sup) * len(data)):
+            prune[k] = v
+
+    return prune
+
+
+def freq_sets(data):
+    ''' 
+    Generates frequent item sets, association rules and confidence values and writes them to a file
+    '''
+    candidateDict = dict(first(data))
+    g_dict = candidateDict
+    first_iteration = 1
+
+    while len(candidateDict) != 0:
+        c = []
+        if first_iteration == 1:
+            first_iteration = 0
+            for item in candidateDict:
+                for item1 in candidateDict:
+                    if item1 > item:
+                        c.append((item, item1))
+        else:
+            for item1 in candidateDict:
+                for item2 in candidateDict:
+                    if (item2[-1] != item1[-1]) and (item1[:-1] == item2[:-1]):
+                        if item1[-1] > item2[-1]:
+                            c.append(item2 + (item1[-1],))
+                        else:
+                            c.append(item1 + (item2[-1],))
+
+        candidateDict = item_sets(c, data)
+
+        g_dict.update(candidateDict)
+
+    out_buffer = []
+    out_buffer.append('-' * 70 + '\n')
+    out_buffer.append('1. User Input:' + '\n')
+    out_buffer.append('\n')
+    out_buffer.append('Support=' + str(min_sup) + '\n')
+    out_buffer.append('Confidence=' + str(min_conf) + '\n')
+    out_buffer.append('\n')
+    out_buffer.append('2. Rules:' + '\n')
+    out_buffer.append('\n')
+    write_data('Rules.txt', out_buffer)
+    out_buffer.clear()
+
+    rule_no = 1
+    for k, v in g_dict.items():
+        if type(k) == tuple:
+
+            for j in range(1, len(k)):
+                combinations = list(itertools.combinations(k, j))
+                for combination in combinations:
+                    if len(combination) != 1:
+                        if combination in g_dict:
+                            c = v / g_dict[combination]
+                    else:
+                        if combination[0] in g_dict:
+                            c = v / g_dict[combination[0]]
+
+                    if c >= min_conf:
+                        key_list = list(k)
+                        for r in list(combination):
+                            key_list.remove(r)
+
+                        new_list = list(combination)
+                        for key in list(key_list):
+                            new_list.append(key)
+                        sup = getSupport(new_list, data)
+
+                        out_buffer.append('Rule#' + str(rule_no) + ': ' + '{' + getOutputString(list(combination)) + '}'
+                                          + '=>' + '{' + getOutputString(list(key_list)) + '}' + '\n')
+                        out_buffer.append('(Support=' + str(round(sup, 2)) + ', ' +
+                                          'Confidence=' + str(round(c, 2)) + ')' + '\n')
+                        out_buffer.append('\n')
+                        write_data('Rules.txt', out_buffer)
+                        out_buffer.clear()
+                        rule_no += 1
+
+
+def getSupport(items, data):
+    '''
+    Returns the support of a given rule
+    '''
+    count = 0
+    for row in data:
+        if set(items).issubset(set(row)):
+            count += 1
+
+    return count / len(data)
+
+
+def getOutputString(list_combination):
+    '''
+    Returns a formatted string of the rule to be written to file
+    '''
+    out = ''
+
+    for i in range(0, len(list_combination)):
+        item = list_combination[i]
+
+        if item == 'sunny' or item == 'overcast' or item == 'rain':
+            out += 'Outlook=' + item
+        elif item == 'hot' or item == 'mild' or item == 'cool':
+            out += 'Temperature=' + item
+        elif item == 'high' or item == 'normal':
+            out += 'Humidity=' + item
+        elif item == 'TRUE' or item == 'FALSE':
+            out += 'Windy=' + item
+        elif item == 'P' or item == 'N':
+            out += 'PlayTennis=' + item
+
+        if i != len(list_combination) - 1:
+            out += ','
+    return out
+
+
+def main():
+
+    data = read_data('Play_Tennis_Data_Set.csv')
+
+    global min_sup, min_conf, g_dict
+    min_sup = float(input("Enter minimum support value: "))
+    min_conf = float(input("Enter minimum confidence value: "))
+
+    freq_sets(data)
+
+
+if __name__ == "__main__":
+    main()
